c87d70ed739110f2d0f814d09e149938f463af98
Takio W <oswini@Oswins-MacBook-Pro.local>
2018-06-10 21:35:11 -0400

    course editor, course list, course manager, lessontabs, moduleeditor list and topicpills added

---
+++ course-manager-client-react/src/containers/CourseManager.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CourseList from './CourseList';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import CourseEditor from './CourseEditor';
+
+
+class CourseManager extends React.Component {
+   render() {
+       return (
+         <Router>
+            <div>
+           <h1>Course Manager</h1>
+           <Route path="/courses"
+              component={CourseList}/>
+           <Route path="/course/:courseId/edit"
+                component={CourseEditor}/>
+
+           
+          </div>
+         </Router>
+       )
+   }
+}
+export default CourseManager;
